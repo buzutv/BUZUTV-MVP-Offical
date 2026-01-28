aaf86c7b1851f63e7461cdb50ec80912bb9d73f6
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2026-01-28 15:44:17 +0300

    fix/Videoplayer size fix

--- src/components/FullscreenPlayer.tsx
+++ src/components/FullscreenPlayer.tsx
@@ -514,7 +514,7 @@ const FullscreenPlayer = ({
 
 
       <div className="w-full mx-auto p-8 h-full overflow-y-auto">
-        <div className="w-full x-auto px-4 py-12">
+        <div className="max-w-6xl mx-auto px-4 py-12">
           <div className="flex justify-center items-center gap-4 mb-4">
             <div className="flex items-center justify-start gap-4 cursor-pointer flex-1" onClick={async () => {
               console.log("🔙 [FullscreenPlayer] Back button clicked, saving and refetching...");
